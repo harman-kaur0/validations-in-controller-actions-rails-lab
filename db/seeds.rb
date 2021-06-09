7c4b59d816cc825a189194f41c390e6d047b1b6d
Harmandeep Kaur <harmandeepk628@gmail.com>
2021-06-09 16:00:05 -0700

    commit tree 1b3c22815fc441c79630bbf23387e78dce5b772a

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,6 @@
+Author.destroy_all
+Post.destroy_all
+
 Author.create!(
   name: "Ada Lovelace", email: "ada@the-history-of-programming.com"
 )
@@ -7,3 +10,5 @@ Post.create!(
   content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed dapibus, nulla vel condimentum ornare, arcu lorem hendrerit purus, ac sagittis ipsum nisl nec erat. Morbi porta sollicitudin leo, eu cursus libero posuere ac. Sed ac ultricies ante. Donec nec nulla ipsum. Nunc eleifend, ligula ut volutpat.",
   category: "Non-Fiction"
 )
+
+puts "*******"
